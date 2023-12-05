9ebedf368f1daefe52c7b7edf4196e50b6afcb47
Rashid Ramazanov <rasid.ramazanov@mobven.com>
2023-12-05 10:38:57 +0300

    Update URLSession+Extension.swift

--- Sources/MBVersion/URLSession+Extension.swift
+++ Sources/MBVersion/URLSession+Extension.swift
@@ -10,36 +10,43 @@ import Foundation
 extension URLSession {
     /// Swizzles URLSession.dataTask(with:completionHandler:) with call of `MBVersion.shared.show()`
     class func swizzleDataTask() {
-        let originalSelector = #selector(
-            URLSession.dataTask(with:completionHandler:) as (URLSession) -> (
-                URLRequest, @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
-            ) -> URLSessionDataTask
-        )
-        let swizzledSelector = #selector(
-            URLSession.dcDataTask(with:completionHandler:) as (URLSession) -> (
-                URLRequest, @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
-            ) -> URLSessionDataTask
-        )
-
-        guard let originalMethod = class_getInstanceMethod(self, originalSelector),
-              let swizzledMethod = class_getInstanceMethod(self, swizzledSelector) else { return }
-
-        let didAddMethod = class_addMethod(
-            self,
-            originalSelector,
-            method_getImplementation(swizzledMethod),
-            method_getTypeEncoding(swizzledMethod)
-        )
-
-        if didAddMethod {
-            class_replaceMethod(
-                self,
-                swizzledSelector,
-                method_getImplementation(originalMethod),
-                method_getTypeEncoding(originalMethod)
+        /*
+         Swizzling should always be done in a dispatch_once,
+         since GCD’s dispatch_once provides both atomicity (i.e. all or nothing)
+         and guarantee that code will be executed exactly once, even across different threads.
+         */
+        DispatchQueue.once {
+            let originalSelector = #selector(
+                URLSession.dataTask(with:completionHandler:) as (URLSession) -> (
+                    URLRequest, @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
+                ) -> URLSessionDataTask
             )
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod)
+            let swizzledSelector = #selector(
+                URLSession.dcDataTask(with:completionHandler:) as (URLSession) -> (
+                    URLRequest, @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
+                ) -> URLSessionDataTask
+            )
+
+            guard let originalMethod = class_getInstanceMethod(self, originalSelector),
+                  let swizzledMethod = class_getInstanceMethod(self, swizzledSelector) else { return }
+
+            let didAddMethod = class_addMethod(
+                self,
+                originalSelector,
+                method_getImplementation(swizzledMethod),
+                method_getTypeEncoding(swizzledMethod)
+            )
+
+            if didAddMethod {
+                class_replaceMethod(
+                    self,
+                    swizzledSelector,
+                    method_getImplementation(originalMethod),
+                    method_getTypeEncoding(originalMethod)
+                )
+            } else {
+                method_exchangeImplementations(originalMethod, swizzledMethod)
+            }
         }
     }
 
@@ -50,6 +57,8 @@ extension URLSession {
     ) -> URLSessionDataTask {
         // Will call the original dcDataTask(:) method which is at this point swizzled
         print("MBVersion: \(request.url?.absoluteString ?? "")")
-        return dcDataTask(with: request, completionHandler: completionHandler)
+        return dcDataTask(with: request) { data, response, error in
+            completionHandler(data, response, error)
+        }
     }
 }
