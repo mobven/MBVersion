5bb7253aa92b12b73fdddc2d672ab613677adbe6
Rashid Ramazanov <rasid.ramazanov@mobven.com>
2023-12-05 10:29:11 +0300

    Update URLSession+Extension.swift

--- Sources/MBVersion/URLSession+Extension.swift
+++ Sources/MBVersion/URLSession+Extension.swift
@@ -10,43 +10,36 @@ import Foundation
 extension URLSession {
     /// Swizzles URLSession.dataTask(with:completionHandler:) with call of `MBVersion.shared.show()`
     class func swizzleDataTask() {
-        /*
-         Swizzling should always be done in a dispatch_once,
-         since GCD’s dispatch_once provides both atomicity (i.e. all or nothing)
-         and guarantee that code will be executed exactly once, even across different threads.
-         */
-        DispatchQueue.once {
-            let originalSelector = #selector(
-                URLSession.dataTask(with:completionHandler:) as (URLSession) -> (
-                    URLRequest, @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
-                ) -> URLSessionDataTask
-            )
-            let swizzledSelector = #selector(
-                URLSession.dcDataTask(with:completionHandler:) as (URLSession) -> (
-                    URLRequest, @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
-                ) -> URLSessionDataTask
-            )
+        let originalSelector = #selector(
+            URLSession.dataTask(with:completionHandler:) as (URLSession) -> (
+                URLRequest, @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
+            ) -> URLSessionDataTask
+        )
+        let swizzledSelector = #selector(
+            URLSession.dcDataTask(with:completionHandler:) as (URLSession) -> (
+                URLRequest, @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
+            ) -> URLSessionDataTask
+        )
 
-            guard let originalMethod = class_getInstanceMethod(self, originalSelector),
-                  let swizzledMethod = class_getInstanceMethod(self, swizzledSelector) else { return }
+        guard let originalMethod = class_getInstanceMethod(self, originalSelector),
+              let swizzledMethod = class_getInstanceMethod(self, swizzledSelector) else { return }
 
-            let didAddMethod = class_addMethod(
+        let didAddMethod = class_addMethod(
+            self,
+            originalSelector,
+            method_getImplementation(swizzledMethod),
+            method_getTypeEncoding(swizzledMethod)
+        )
+
+        if didAddMethod {
+            class_replaceMethod(
                 self,
-                originalSelector,
-                method_getImplementation(swizzledMethod),
-                method_getTypeEncoding(swizzledMethod)
+                swizzledSelector,
+                method_getImplementation(originalMethod),
+                method_getTypeEncoding(originalMethod)
             )
-
-            if didAddMethod {
-                class_replaceMethod(
-                    self,
-                    swizzledSelector,
-                    method_getImplementation(originalMethod),
-                    method_getTypeEncoding(originalMethod)
-                )
-            } else {
-                method_exchangeImplementations(originalMethod, swizzledMethod)
-            }
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod)
         }
     }
 
