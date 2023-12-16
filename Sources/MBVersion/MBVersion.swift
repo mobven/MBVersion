3ea024d1a246d41fb8cbc133f3a81e376776d389
Furkan Erucar <furkan.erucar@mobven.com>
2023-12-16 16:41:33 +0300

    clickable version label and presenting details of the page and networking page

--- Sources/MBVersion/MBVersion.swift
+++ Sources/MBVersion/MBVersion.swift
@@ -13,7 +13,18 @@ public final class MBVersion {
     /// `MBVersion` singleton instance.
     public static let shared = MBVersion()
 
-    lazy var versionLabel: UILabel = {
+    let backgroundColor = UIColor(red: .zero, green: 115 / 255, blue: 239 / 255, alpha: 1)
+
+    private init() {
+        DispatchQueue.main.async {
+            UIViewController.swizzleWillAppear()
+            UIApplication.shared.keyWindow?.addSubview(self.versionLabel)
+            self.setVersionLabelText()
+            self.versionLabel.addGestureRecognizer(self.addGesture())
+        }
+    }
+
+    private lazy var versionLabel: UILabel = {
         let label = UILabel()
         label.frame = CGRect(
             x: 0,
@@ -21,19 +32,12 @@ public final class MBVersion {
             width: UIScreen.main.bounds.width,
             height: 20
         )
-        label.backgroundColor = UIColor(red: .zero, green: 115 / 255, blue: 239 / 255, alpha: 1)
+        label.backgroundColor = backgroundColor
         label.textColor = UIColor.white
         label.font = UIFont.boldSystemFont(ofSize: 13)
+        label.isUserInteractionEnabled = true
         return label
     }()
-
-    private init() {
-        DispatchQueue.main.async {
-            UIViewController.swizzleWillAppear()
-            UIApplication.shared.keyWindow?.addSubview(self.versionLabel)
-            self.setVersionLabelText()
-        }
-    }
 
     private func setVersionLabelText() {
         guard let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
@@ -58,5 +62,37 @@ public final class MBVersion {
         UIApplication.shared.keyWindow?.frame = CGRect(
             origin: .zero, size: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 20)
         )
+    }
+}
+
+extension MBVersion {
+    private func addGesture() -> UITapGestureRecognizer {
+        let versionLabelGestureSelector = #selector(onTapVersionLabel(_:))
+
+        let gesture = UITapGestureRecognizer(target: self, action: versionLabelGestureSelector)
+        return gesture
+    }
+
+    @objc func onTapVersionLabel(_ sender: UITapGestureRecognizer) {
+        let storyboard = UIStoryboard(name: "VersionDetails", bundle: .module)
+        guard let controller = storyboard.instantiateViewController(
+            withIdentifier: "VersionDetailsViewController"
+        ) as? VersionDetailsViewController else { return }
+        controller.snapShot = takeScreenshot()
+        controller.view.backgroundColor = backgroundColor
+        UIApplication.shared.keyWindow?.rootViewController?.present(controller, animated: true)
+    }
+}
+
+extension MBVersion {
+    func takeScreenshot() -> UIImage? {
+        guard let window = UIApplication.shared.windows.first else { return nil }
+
+        let renderer = UIGraphicsImageRenderer(size: window.bounds.size)
+        let snapshot = renderer.image { _ in
+            window.drawHierarchy(in: window.bounds, afterScreenUpdates: true)
+        }
+
+        return snapshot
     }
 }
